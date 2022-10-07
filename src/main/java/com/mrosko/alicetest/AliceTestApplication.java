4b4b5ff6b13966d5c2c98b9684cb3760f14a9cf5
mroskino <michal.mrosko@gmail.com>
2022-10-07 16:29:01 +0200

    Init

---
+++ src/main/java/com/mrosko/alicetest/AliceTestApplication.java
@@ -0,0 +1,13 @@
+package com.mrosko.alicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrosko/alicetest/AliceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrosko.alicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
